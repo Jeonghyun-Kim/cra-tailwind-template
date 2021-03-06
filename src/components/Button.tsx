caf669c6d223a058468bc3d4434941133d5f6948
Kay Kim <>
2021-03-06 23:29:21 +0900

    add Tailwindui Components - Dropdown - Modal - Notification - Select - toggle
    upgrade Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: React.ReactNode;
+  color?: 'indigo' | 'red';
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
@@ -11,7 +12,9 @@ const Button: React.FC<Props> = ({
   className,
   children,
   disabled,
+  color = 'indigo',
   size = 'md',
+  ...props
 }) => {
   return (
     <button
@@ -25,11 +28,12 @@ const Button: React.FC<Props> = ({
           'px-4 py-2 text-sm rounded-md': size === 'md',
           'px-4 py-2 text-base rounded-md': size === 'lg',
           'px-6 py-3 text-base rounded-md': size === 'xl',
-          'text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500': !disabled,
+          [`text-white bg-${color}-600 hover:bg-${color}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-${color}-500`]: !disabled,
           'text-gray-50 bg-gray-400 opacity-70 cursor-default': disabled,
         },
       )}
       disabled={disabled}
+      {...props}
     >
       {children}
     </button>
