1ce152c1cda8be2f40974f6f54d1491c38bc1ff0
Kay Kim <kimjh@bawi.org>
2021-04-15 22:03:29 +0900

    use tailwindcss jit
    use @headlessui/react v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import Button from 'components/Button';
-
-const Root = styled.div``;
+import Toggle from 'components/Toggle';
 
 const App: React.FC = () => {
+  const [enabled, setEnabled] = React.useState<boolean>(false);
+
   return (
-    <Root className="max-w-7xl mx-auto p-2">
-      <div>hello world</div>
-      <div>
-        <Button size="lg">click me!</Button>
-      </div>
-      <div>
-        <Button size="lg" disabled>
-          disabled
-        </Button>
-      </div>
-    </Root>
+    <div className="max-w-7xl mx-auto p-2">
+      <Toggle enabled={enabled} onChange={setEnabled} srLabel="test toggle" />
+    </div>
   );
 };
 
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,128 +1,40 @@
 import React from 'react';
+import { Switch } from '@headlessui/react';
 import cn from 'classnames';
 
 interface Props {
-  className?: string;
-  enabled: boolean;
-  size?: 'sm' | 'md';
-  color?: 'indigo' | 'red';
-  onChange: (enabled: boolean) => void;
-  withIcon?: boolean;
+  srLabel?: string;
 }
 
-const Toggle: React.FC<Props> = ({
-  className,
+interface ControledProps extends Props {
+  enabled: boolean;
+  onChange: (enabled: boolean) => void;
+}
+
+const ControledToggle: React.FC<ControledProps> = ({
+  srLabel,
   enabled,
-  size = 'md',
-  color = 'indigo',
   onChange,
-  withIcon = false,
-  ...props
 }) => {
   return (
-    <button
-      type="button"
+    <Switch
       className={cn(
-        className,
-        'relative inline-flex flex-shrink-0 rounded-full cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2',
-        `focus:ring-${color}-500`,
-        {
-          'h-5 w-10 group items-center justify-center ': size === 'sm',
-          'h-6 w-11 border-2 border-transparent transition-colors ease-in-out duration-200 ':
-            size === 'md',
-        },
-        {
-          [`bg-${color}-600`]: enabled,
-          'bg-gray-200': !enabled,
-        },
+        enabled ? 'bg-indigo-500' : 'bg-gray-200',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full transition-colors ease-in-out duration-200',
       )}
-      onClick={() => onChange(!enabled)}
-      aria-pressed={enabled ? 'true' : 'false'}
-      {...props}
+      checked={enabled}
+      onChange={onChange}
     >
-      <span className="sr-only">Use setting</span>
-      {size === 'sm' && (
-        <>
-          <span
-            aria-hidden="true"
-            className="pointer-events-none absolute bg-white w-full h-full rounded-md"
-          />
-          <span
-            aria-hidden="true"
-            className={cn(
-              'pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200',
-              {
-                [`bg-${color}-600`]: enabled,
-                'bg-gray-200': !enabled,
-              },
-            )}
-          />
-        </>
-      )}
+      {srLabel && <span className="sr-only">{srLabel}</span>}
       <span
         aria-hidden="true"
         className={cn(
-          'pointer-events-none flex justify-center items-center h-5 w-5 rounded-full bg-white shadow transform ring-0 ease-in-out duration-200',
-          {
-            'absolute left-0 border border-gray-200 transition-transform':
-              size === 'sm',
-            transition: size === 'md',
-          },
-          {
-            'translate-x-5': enabled,
-            'translate-x-0': !enabled,
-          },
+          enabled ? 'translate-x-5' : 'translate-x-0',
+          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
         )}
-      >
-        {withIcon && (
-          <>
-            <span
-              className={cn(
-                'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-                {
-                  'opacity-0 ease-out duration-100': enabled,
-                  'opacity-100 ease-in duration-200': !enabled,
-                },
-              )}
-              aria-hidden="true"
-            >
-              <svg
-                className="bg-white h-3 w-3 text-gray-400"
-                fill="none"
-                viewBox="0 0 12 12"
-              >
-                <path
-                  d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </span>
-            <span
-              className={cn(
-                'opacity-0 ease-out duration-100 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-                {
-                  'opacity-100 ease-in duration-200': enabled,
-                  'opacity-0 ease-out duration-100': !enabled,
-                },
-              )}
-              aria-hidden="true"
-            >
-              <svg
-                className={`bg-white h-3 w-3 text-${color}-600`}
-                fill="currentColor"
-                viewBox="0 0 12 12"
-              >
-                <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-              </svg>
-            </span>
-          </>
-        )}
-      </span>
-    </button>
+      />
+    </Switch>
   );
 };
 
-export default Toggle;
+export default ControledToggle;
